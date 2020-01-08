bcd885b47dfc9d2dca20078f87e0f3fbe962b528
Rob Pike <r@golang.org>
2020-01-08 15:20:52 +1100

    licenses: add Apache-2.0-User, the text that Apache-2.0 suggests
    At the end of the the Apache-2.0 license text is a section that
    says, in effect, "use the following text". Most quote the whole
    license, but some, such as github.com/jmespath/go-jmespath, do as
    directed.
    
    Add that text as a separate license, which we call Apache-2.0-User.

--- data.gen.go
+++ data.gen.go
@@ -6,6 +6,7 @@ func init() {
 	licenses = []license{
 		{name: "AGPL-3.0", doc: normalize([]byte(license_AGPL_3_0))},
 		{name: "Apache-2.0", doc: normalize([]byte(license_Apache_2_0))},
+		{name: "Apache-2.0-User", doc: normalize([]byte(license_Apache_2_0_User))},
 		{name: "Artistic-2.0", doc: normalize([]byte(license_Artistic_2_0))},
 		{name: "BSD-0-Clause", doc: normalize([]byte(license_BSD_0_Clause))},
 		{name: "BSD-2-Clause", doc: normalize([]byte(license_BSD_2_Clause))},
@@ -762,6 +763,14 @@ APPENDIX: How to apply the Apache License to your work.
 To apply the Apache License to your work, attach the following boilerplate notice, with the fields enclosed by brackets "[]" replaced with your own identifying information. (Don't include the brackets!)  The text should be enclosed in the appropriate comment syntax for the file format. We also recommend that a file or class name and description of purpose be included on the same "printed page" as the copyright notice for easier identification within third-party archives.
 
 Copyright [yyyy] [name of copyright owner]
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+`
+const license_Apache_2_0_User = `Copyright [yyyy] [name of copyright owner]
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
 
