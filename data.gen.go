158d942212ef0458acac90ba941882825d1b6896
Russ Cox <rsc@golang.org>
2020-07-10 14:45:26 -0400

    licenses: add NIST
    From https://www.nist.gov/topics/data/public-access-nist-research/copyright-fair-use-and-licensing-statements-srd-data-and#software.
    Test case from https://github.com/usnistgov/ndn-dpdk/blob/master/LICENSE.md
    (includes one extra line from the nist.gov page that isn't technically part of the license).

--- data.gen.go
+++ data.gen.go
@@ -80,6 +80,7 @@ func init() {
 		{Name: "MPL-2.0", Text: license_MPL_2_0},
 		{Name: "MPL-2.0-Header", Text: license_MPL_2_0_Header},
 		{Name: "NCSA", Text: license_NCSA},
+		{Name: "NIST", Text: license_NIST},
 		{Name: "OSL-3.0", Text: license_OSL_3_0},
 		{Name: "OpenSSL", Text: license_OpenSSL},
 		{Name: "Unlicense", Text: license_Unlicense},
@@ -12733,6 +12734,33 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimers in the documentation and/or other materials provided with the distribution.
 * Neither ___ may be used to endorse or promote products derived from this Software without specific prior written permission.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+`
+const license_NIST = `This software was developed by employees of the National Institute of
+Standards and Technology (NIST), an agency of the Federal Government
+and is being made available as a public service. Pursuant to title 17
+United States Code Section 105, works of NIST employees are not
+subject to copyright protection in the United States. This software
+may be subject to foreign copyright. Permission in the United States
+and in foreign countries, to the extent that NIST may hold copyright,
+to use, copy, modify, create derivative works, and distribute this
+software and its documentation without fee is hereby granted on a
+non-exclusive basis, provided that this notice and disclaimer of
+warranty appears in all copies.
+
+THE SOFTWARE IS PROVIDED 'AS IS' WITHOUT ANY WARRANTY OF ANY KIND,
+EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED
+TO, ANY WARRANTY THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS, ANY
+IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, AND FREEDOM FROM INFRINGEMENT, AND ANY WARRANTY THAT THE
+DOCUMENTATION WILL CONFORM TO THE SOFTWARE, OR ANY WARRANTY THAT THE
+SOFTWARE WILL BE ERROR FREE. IN NO EVENT SHALL NIST BE LIABLE FOR ANY
+DAMAGES, INCLUDING, BUT NOT LIMITED TO, DIRECT, INDIRECT, SPECIAL OR
+CONSEQUENTIAL DAMAGES, ARISING OUT OF, RESULTING FROM, OR IN ANY WAY
+CONNECTED WITH THIS SOFTWARE, WHETHER OR NOT BASED UPON WARRANTY,
+CONTRACT, TORT, OR OTHERWISE, WHETHER OR NOT INJURY WAS SUSTAINED BY
+PERSONS OR PROPERTY OR OTHERWISE, AND WHETHER OR NOT LOSS WAS
+SUSTAINED FROM, OR AROSE OUT OF THE RESULTS OF, OR USE OF, THE
+SOFTWARE OR SERVICES PROVIDED HEREUNDER.
 `
 const license_OSL_3_0 = `This Open Software License (the "License") applies to any original work of authorship (the "Original Work") whose owner (the "Licensor") has placed the following licensing notice adjacent to the copyright notice for the Original Work:
 
