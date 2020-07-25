51e5447c058daff79868daea26995f73a6635332
Russ Cox <rsc@golang.org>
2020-07-24 20:42:14 -0400

    licenses: add Unlicense [LRE]

--- data.gen.go
+++ data.gen.go
@@ -145,6 +145,7 @@ func init() {
 		{Name: "OpenSSL", Text: license_OpenSSL_lre},
 		{Name: "PHP-3.0", Text: license_PHP_3_0_lre},
 		{Name: "PHP-3.01", Text: license_PHP_3_01_lre},
+		{Name: "Unlicense", Text: license_Unlicense_lre},
 	}
 	builtinListLRE = filesLRE // TODO URLs
 }
@@ -25749,4 +25750,33 @@ For more information on the PHP Group and the PHP project, please see
 
 PHP includes the Zend Engine, freely available at <http:/www.zend.com>.
 ))??
+`
+const license_Unlicense_lre = `//**
+The Unlicense
+https://spdx.org/licenses/Unlicense.json
+https://unlicense.org/
+**//
+
+((This))??
+is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this
+software, either in source code form or as a compiled binary, for any purpose,
+commercial or non-commercial, and by any means.
+
+In jurisdictions that recognize copyright laws, the author or authors of this
+software dedicate any and all copyright interest in the software to the public
+domain. We make this dedication for the benefit of the public at large and to
+the detriment of our heirs and successors. We intend this dedication to be an
+overt act of relinquishment in perpetuity of all present and future rights to
+this software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+(( For more information, please refer to <https:/unlicense.org/> ))??
 `
