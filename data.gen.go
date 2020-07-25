b901cc9caf4344c6483fe0af9357444b4810439b
Russ Cox <rsc@golang.org>
2020-07-24 23:09:52 -0400

    licenses: add ISC license [LRE]

--- data.gen.go
+++ data.gen.go
@@ -168,6 +168,7 @@ func init() {
 		{Name: "GPL-3.0", Text: license_GPL_3_0_lre},
 		{Name: "GPL-3.0-Only", Text: license_GPL_3_0_Only_lre},
 		{Name: "IPL-1.0", Text: license_IPL_1_0_lre},
+		{Name: "ISC", Text: license_ISC_lre},
 		{Name: "JSON", Text: license_JSON_lre},
 		{Name: "LGPL-2.0", Text: license_LGPL_2_0_lre},
 		{Name: "LGPL-2.0-Only", Text: license_LGPL_2_0_Only_lre},
@@ -33516,6 +33517,38 @@ OF THE PROGRAM CONSTITUTES RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT.
    Agreement will bring a legal action under this Agreement more than one year
    after the cause of action arose. Each party waives its rights to a jury trial
    in any resulting litigation.
+`
+const license_ISC_lre = `//**
+ISC License
+https://spdx.org/licenses/ISC.json
+https://www.isc.org/downloads/software-support-policy/isc-license/
+https://opensource.org/licenses/ISC
+**//
+
+((
+ISC License
+((Copyright __20__))??
+))??
+
+Permission to use, copy, modify, and
+(( /or ))??
+distribute this software for any purpose with or without fee is hereby granted,
+provided that the above
+(( copyright ))??
+notice and this permission notice appear in
+all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND
+//** ISC **//
+__5__
+DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+//** ISC **//
+__5__
+BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 `
 const license_JSON_lre = `
 ((JSON license))??
