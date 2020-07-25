71a66a944a2d5078d1b75cdfd80fac830680c339
Russ Cox <rsc@golang.org>
2020-07-24 20:45:01 -0400

    licenses: add NCSA Open Source License [LRE]

--- data.gen.go
+++ data.gen.go
@@ -138,6 +138,7 @@ func init() {
 		{Name: "MPL-1.0", Text: license_MPL_1_0_lre},
 		{Name: "MPL-1.1", Text: license_MPL_1_1_lre},
 		{Name: "MPL-2.0", Text: license_MPL_2_0_lre},
+		{Name: "NCSA", Text: license_NCSA_lre},
 		{Name: "OpenSSL", Text: license_OpenSSL_lre},
 		{Name: "PHP-3.0", Text: license_PHP_3_0_lre},
 		{Name: "PHP-3.01", Text: license_PHP_3_01_lre},
@@ -24419,6 +24420,48 @@ This Source Code Form is "Incompatible With Secondary Licenses", as defined by
 the Mozilla Public License, v. 2.0. ))??
 
 ))
+`
+const license_NCSA_lre = `//**
+University of Illinois/NCSA Open Source License
+https://spdx.org/licenses/NCSA.json
+http://otm.illinois.edu/uiuc_openSource
+https://opensource.org/licenses/NCSA
+**//
+
+(( University of Illinois/NCSA Open Source License
+(( Copyright __20__ ))??
+))??
+
+((Developed by: __30__))??
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal with
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+   *
+   Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimers.
+
+   *
+   Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimers in the documentation
+   and/or other materials provided with the distribution.
+
+   *
+   Neither the names of __10__
+   nor the names of its contributors may be used to endorse or promote
+   products derived from this Software without specific prior written
+   permission.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE CONTRIBUTORS
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
 `
 const license_OpenSSL_lre = `//**
 OpenSSL License
